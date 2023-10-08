1f3c231b4f64fd110881bb001feb511fab0fb4a0
Elton João Zierhut <eltonzierhut@gmail.com>
2023-10-08 11:46:18 -0300

    refac: add authentication to LoginForm

--- src/pages/LoginForm/LoginForm.js
+++ src/pages/LoginForm/LoginForm.js
@@ -1,43 +1,68 @@
 import React, { useState } from 'react';
-import { Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  VStack,
+} from '@chakra-ui/react';
 import AuthService from '../../services/AuthService';
 
 export function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
   const handleLogin = async () => {
     try {
       const response = await AuthService.login(email, password);
-      console.log('Token JWT recebido:', response.data.token);
-      // Aqui você pode adicionar lógica para armazenar o token em um estado global ou local.
+      sessionStorage.setItem('token', response.data.token);
+      window.location.href = '/home';
     } catch (error) {
       console.error('Erro de autenticação:', error);
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
     }
   };
 
   return (
-    <Stack spacing={3}>
-      <FormControl id="email">
-        <FormLabel>Email</FormLabel>
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="password">
-        <FormLabel>Senha</FormLabel>
-        <Input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormControl>
-      <Button colorScheme="blue" onClick={handleLogin}>
-        Login
-      </Button>
-    </Stack>
+    <VStack spacing={4}>
+      <Stack spacing={3}>
+        <FormControl id="email">
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel>Senha</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </FormControl>
+        <Button colorScheme="blue" onClick={handleLogin}>
+          Login
+        </Button>
+      </Stack>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>Erro de Autenticação!</AlertTitle>
+          <AlertDescription>Usuário ou senha incorretos.</AlertDescription>
+        </Alert>
+      )}
+    </VStack>
   );
 }
 
